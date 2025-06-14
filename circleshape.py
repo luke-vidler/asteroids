be5d622fa6f7c522996ff568fc90e5a56c4b489e
Luke Vidler <luke.vidler@gmail.com>
2025-06-14 15:21:16 +1000

    working player triangle

--- circleshape.py
+++ circleshape.py
@@ -15,8 +15,15 @@ class CircleShape(pygame.sprite.Sprite):
 
     def draw(self, screen):
         # sub-classes must override
-        # pygame.draw.polygon(screen, "white", self.triangle(), width=2)
-        pass
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
 
     def update(self, dt):
         # sub-classes must override
--- player.py
+++ player.py
@@ -7,19 +7,10 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-# in the player class
-def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+    def rotate(self, dt):        
+        self.rotation += PLAYER_TURN_SPEED * dt
 
-def rotate(self, dt):        
-    self.rotation += PLAYER_TURN_SPEED * dt
-
-def update(self, dt):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
