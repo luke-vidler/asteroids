051460fb7d27e9a3c743a6092fa7f81e5d67841c
Luke Vidler <luke.vidler@gmail.com>
2025-06-14 15:07:57 +1000

    update

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
+        dt = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(("black"))
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
